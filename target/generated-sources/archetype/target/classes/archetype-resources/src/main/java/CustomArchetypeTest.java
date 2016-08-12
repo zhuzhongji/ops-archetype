fd8bc22aec78ed9a2d9f0d2ae1074d19ff53a5fc
zhuzhongji <jizhongzhu@126.com>
2016-08-12 15:42:02 +0800

    first commit

---
+++ target/generated-sources/archetype/target/classes/archetype-resources/src/main/java/CustomArchetypeTest.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * custom archetype test class template
+ * @author: zhuzhongji 
+ * @date:   2016-8-12 上午11:33:52
+ */
+public class CustomArchetypeTest {
+
+	/**
+	 * 
+	 * @author: zhuzhongji 
+	 * @date:   2016-8-12 上午11:35:39 
+	 * @param:   
+	 * @return: void
+	 */
+	public void test() {
+		System.out.println("custom archetype test method template");
+	}
+	
+	/**
+	 * 
+	 * @author: zhuzhongji 
+	 * @date:   2016-8-12 上午11:35:43 
+	 * @param: @param args  
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
